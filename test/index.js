9ef0035d16fe9c23a2079830d3b525fd2eb773cc
Brad Davis <bradrobertdavis@gmail.com>
2015-04-14 02:15:57 -0700

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,15 +1,28 @@
 var ebay = require('../index');
 var conf = require('./config');
 
+console.log(conf.RuName);
+
 var params = {
-  callname: 'GeteBayDetails',
+  callname: 'GetSessionID',
   siteid: 0,
   data: {
-    RequesterCredentials: {
-      eBayAuthToken: conf.eBayAuthToken
-    }
+    RuName: conf.RuName
   }
 }
+
+//TODO: Go here via browser
+//https://signin.sandbox.ebay.com/ws/eBayISAPI.dll?SignIn&runame=HashtagSell__In-HashtagS-e6d2-4-sdojf&SessID=SESSION ID HERE
+
+
+//var params = {
+//  callname: 'FetchToken',
+//  siteid: 0,
+//  data: {
+//    SessionID: [SESSION ID HERE TO GET TOKEN],
+//  }
+//}
+
 
 ebay.call(params, function(err, result) {
   console.dir(result);
